5c1e13ded22331fee22859e76adcf9aadcf5c0d2
mvpc-11232 <mvp2612@hotmail.com>
2022-12-23 09:04:52 +0530

    Auto Push Commit

--- ec2.tf
+++ ec2.tf
@@ -6,7 +6,7 @@ resource "aws_spot_instance_request" "spot" {
   instance_type                = var.INSTANCE_TYPE
   wait_for_fulfillment         = true
   vpc_security_group_ids       = [aws_security_group.allow_app.id]
-  subnet_id                    = 
+  subnet_id                    = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_ID, count.index)
 
   tags = {
     Name = "${var.COMPONENT}-${var.ENV}"
