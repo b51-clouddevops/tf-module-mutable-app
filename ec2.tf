6f45c337ca22c22cff6f655c3b0c278816097420
mvpc-11232 <mvp2612@hotmail.com>
2022-12-23 08:55:31 +0530

    Auto Push Commit

--- ec2.tf
+++ ec2.tf
@@ -26,3 +26,25 @@ resource "aws_spot_instance_request" "spot" {
 }
 
 # Creates On-Demand-Server
+resource "aws_instance" "od" {
+  ami                        = data.aws_ami.myami.image_id
+  instance_type              = "t3.micro"
+  vpc_security_group_ids     = [aws_security_group.allows_ssh.id]
+
+  connection {
+    type     = "ssh"
+    user     = "centos"
+    password = "DevOps321"
+    host     = self.private_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "ansible-pull -U https://github.com/b51-clouddevops/ansible.git -e ansible_user=centos -e ansible_password=DevOps321 -e DB_PASSWORD=RoboShop@1 -e COMPONENT=${var.COMPONENT} -e APP_VERSION=${var.APP_VERSION} -e ENV=dev roboshop-pull.yml"
+    ]
+  }
+
+  tags = {
+    Name = "${var.COMPONENT}-robot"
+  }
+}
