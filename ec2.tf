890d6987984bffc7ef39af75c6afc6b4992874be
mvpc-11232 <mvp2612@hotmail.com>
2022-12-23 08:51:06 +0530

    Auto Push Commit

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,6 @@
-Request a spot instance at $0.03
 resource "aws_spot_instance_request" "spot" {
   ami                          = data.aws_ami.myami.image_id
-  instance_type                = "t3.micro"
+  instance_type                = var.INSTANCE_TYPE
   wait_for_fulfillment         = true
   vpc_security_group_ids       = [aws_security_group.allows_ssh.id]
 
