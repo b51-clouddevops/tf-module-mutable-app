5104c7180dab0d0036fc3066519b25f49bc03410
CodingManoj <mvp2612@hotmail.com>
2022-12-25 09:00:58 +0530

    Auto Push Commit

--- route53.tf
+++ route53.tf
@@ -3,6 +3,5 @@ resource "aws_route53_record" "record" {
   name    = "${var.COMPONENT}-dev.${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONENAME}"
   type    = "CNAME"
   ttl     = 10
-  # records = [aws_spot_instance_request.spot.private_ip]
-  records = [aws_instance.app.private_ip]
+  records = 
 }
