afa8fd5f4e4fe5eb816014c26713a85d3947190d
CodingManoj <mvp2612@hotmail.com>
2022-12-25 09:02:01 +0530

    Auto Push Commit

--- route53.tf
+++ route53.tf
@@ -3,5 +3,8 @@ resource "aws_route53_record" "record" {
   name    = "${var.COMPONENT}-dev.${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONENAME}"
   type    = "CNAME"
   ttl     = 10
-  records = 
+  records = var.I
 }
+
+# If the record created is called by backend module, record should be created under private hosted zone
+# If the record created is called by frontend module, record should be created under public hosted zone
