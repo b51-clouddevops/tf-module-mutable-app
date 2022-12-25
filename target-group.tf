02be74e5a9f7078391c1e5411a2559755b3e7824
CodingManoj <mvp2612@hotmail.com>
2022-12-25 09:23:36 +0530

    Auto Push Commit

--- target-group.tf
+++ target-group.tf
@@ -17,7 +17,7 @@ resource "aws_lb_target_group_attachment" "instances-attach" {
 # Adding Rule inside the created listerer
 resource "aws_lb_listener_rule" "app-rule" {
   listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
-  priority     = 100
+  priority     = 
 
   action {
     type             = "forward"
