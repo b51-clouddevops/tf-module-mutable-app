a91e4b5bc97a86192fd2ad08f7f6f86d53649e58
CodingManoj <mvp2612@hotmail.com>
2022-12-25 07:57:04 +0530

    Auto Push Commit

--- app.tf
+++ app.tf
@@ -2,18 +2,18 @@ resource "null_resource" "app" {
 
   count = var.SPOT_INSTANCE_COUNT + var.OD_INSTANCE_COUNT 
 
+
+#   provisioner "remote-exec" {
   connection {
     type     = "ssh"
     user     = jsondecode(data.aws_secretsmanager_secret_version.robot-secrets.secret_string)["SSH_USERNAME"]
     password = jsondecode(data.aws_secretsmanager_secret_version.robot-secrets.secret_string)["SSH_PASSWORD"]
     host     = element(local.ALL_INSTANCE_IPS, count.index)
   }
-
-#   provisioner "remote-exec" {
-#     inline = [
-#       "ansible-pull -U https://github.com/b51-clouddevops/ansible.git -e ansible_user=centos -e ansible_password=DevOps321 -e DB_PASSWORD=RoboShop@1 -e COMPONENT=${var.COMPONENT} -e APP_VERSION=${var.APP_VERSION} -e ENV=dev roboshop-pull.yml"
-#     ]
-#   }
+    inline = [
+      "ansible-pull -U https://github.com/b51-clouddevops/ansible.git -e ansible_user=centos -e ansible_password=DevOps321 -e DB_PASSWORD=RoboShop@1 -e COMPONENT=${var.COMPONENT} -e APP_VERSION=${var.APP_VERSION} -e ENV=dev roboshop-pull.yml"
+    ]
+  }
 }
 
 
