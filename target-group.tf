25cc7c235e83a0da8a4716379f501e809aa42223
CodingManoj <mvp2612@hotmail.com>
2022-12-25 08:48:01 +0530

    Auto Push Commit

--- target-group.tf
+++ target-group.tf
@@ -13,3 +13,26 @@ resource "aws_lb_target_group_attachment" "instances-attach" {
   target_id        = element(local.ALL_INSTANCE_IDS, count.index)
   port             = 8080
 }
+
+# Adding Rule inside the created listerer
+resource "aws_lb_listener_rule" "app-rule" {
+  listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.static.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/static/*"]
+    }
+  }
+
+  condition {
+    host_header {
+      values = ["example.com"]
+    }
+  }
+}
