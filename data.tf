82e669700407346e99d96dbcb2daed7ca451efb3
CodingManoj <mvp2612@hotmail.com>
2022-12-25 07:54:20 +0530

    Auto Push Commit

--- data.tf
+++ data.tf
@@ -24,3 +24,20 @@ data "aws_ami" "myami" {
   owners           = ["self"]
 }
 
+# Fecthing the information of the existing secret which has multiple values
+data "aws_secretsmanager_secret" "robot-secrets" {
+  name = "robot/secrets"
+}
+
+# Fecthing the ID of the secret version of the above secret
+data "aws_secretsmanager_secret_version" "robot-secrets" {
+  secret_id = data.aws_secretsmanager_secret.robot-secrets.id
+}
+
+# Fetching the secret value of the above secret
+output "data" {
+  value = jsondecode(data.aws_secretsmanager_secret_version.robot-secrets.secret_string)["DOCDB_USERNAME"]
+}
+
+
+jsondecode(data.aws_secretsmanager_secret_version.robot-secrets.secret_string)["DOCDB_USERNAME"]
