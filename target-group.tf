d9d883550b743a2f4f4c7bdf911220ed4712f7ff
CodingManoj <mvp2612@hotmail.com>
2022-12-25 08:20:33 +0530

    Auto Push Commit

--- target-group.tf
+++ target-group.tf
@@ -1,6 +1,14 @@
+# Creates the application target group
 resource "aws_lb_target_group" "app" {
   name     = "${var.COMPONENT}-${var.ENV}"
   port     = 8080
   protocol = "HTTP"
   vpc_id   = data.terraform_remote_state.vpc.outputs.VPC_ID
+}
+
+# Attach instances to the target group 
+resource "aws_lb_target_group_attachment" "test" {
+  target_group_arn = aws_lb_target_group.test.arn
+  target_id        = aws_instance.test.id
+  port             = 80
 }
