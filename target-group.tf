bbec35f8b863cef80c1e61301c11c0948ee92452
CodingManoj <mvp2612@hotmail.com>
2022-12-26 07:54:54 +0530

    Auto Push Commit

--- target-group.tf
+++ target-group.tf
@@ -40,8 +40,9 @@ resource "aws_lb_listener_rule" "app-rule" {
 
 
 # Creating a listener in the public-alb 
-resource "aws_lb_listener" "front_end" {
-  load_balancer_arn = aws_lb.front_end.arn
+resource "aws_lb_listener" "public-alb-listener" {
+  count             = var.LB_TYPE == "internal" ? 0 : 1
+  load_balancer_arn = data.terraform_remote_state.alb.outputs.
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
