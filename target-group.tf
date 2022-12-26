93fc99945e5560e4eb20de7a86134b0fad48e3a1
CodingManoj <mvp2612@hotmail.com>
2022-12-26 07:51:58 +0530

    Auto Push Commit

--- target-group.tf
+++ target-group.tf
@@ -40,8 +40,8 @@ resource "aws_lb_listener_rule" "app-rule" {
 
 
 # Adding Rule inside the created private listerer
-resource "aws_lb_listener_rule" "app-rule" {
-  count        = var.LB_TYPE == "internal" ? 1 : 0
+resource "aws_lb_listener_rule" "public-app-rule" {
+  count        = var.LB_TYPE == "internal" ? 0 : 1
   listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
   priority     = random_integer.priority.result   # No two rules should have same priority, hence using the randomly created unique priority number
 
