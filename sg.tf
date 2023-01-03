d1282f183eddd42041825df2363c2fabc0a73fe8
CodingManoj <mvp2612@hotmail.com>
2023-01-03 08:38:14 +0530

    asdf

--- sg.tf
+++ sg.tf
@@ -19,6 +19,14 @@ resource "aws_security_group" "allow_app" {
     cidr_blocks      = [data.terraform_remote_state.vpc.outputs.VPC_CIDR, data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
   }
 
+  ingress {
+    description      = "Prometheus-Port"
+    from_port        = 9100
+    to_port          = 9100
+    protocol         = "tcp"
+    cidr_blocks      = [data.terraform_remote_state.vpc.outputs.VPC_CIDR, data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
